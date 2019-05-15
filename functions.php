01af1ead5bdcea3bdbafe6c20951802456795603
Monirul Islam <monirul.islam.sujon@gmail.com>
2019-05-15 13:31:48 +0600

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Monirul Islam
+ * Author Url: http://www.misujon.com/
+ */
+
+require_once dirname(__FILE__).'/config/constants.php';
+require_once dirname(__FILE__).'/ins/credentials.php';
+require_once dirname(__FILE__).'/ins/mi_mailer.php';
+require_once dirname(__FILE__).'/ins/general.php';
+require_once dirname(__FILE__).'/ins/template.php';
+require_once dirname(__FILE__).'/ins/db_crud.php';
+require_once dirname(__FILE__).'/ins/db_others.php';
+require_once dirname(__FILE__).'/ins/http_req.php';
+require_once dirname(__FILE__).'/ins/mi_printer.php';
